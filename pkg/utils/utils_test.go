7ecea55a64654c4adab0e724e893f469efedb3f2
Damian Cyntler <damian@cyntler.com>
2022-05-07 13:44:06 +0200

    Add second utils test

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -6,6 +6,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSumUtil(t *testing.T) {
-	assert.Equal(t, Sum(10, 10), 21)
+func TestSumUtilValidValue(t *testing.T) {
+	assert.Equal(t, Sum(10, 10), 20)
+}
+
+func TestSumUtilInvalidValue(t *testing.T) {
+	assert.NotEqual(t, Sum(2, 4), 10)
 }
